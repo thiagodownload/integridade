78c5341eb1db140e9117ea453ac15c48078e0b2a
thiagodownload <83071601+thiagodownload@users.noreply.github.com>
2026-08-25 14:20:09 -0300

    test: habilitar leitura GET da configuracao publica

--- api/public/config.ts
+++ api/public/config.ts
@@ -1,5 +1,5 @@
 import { proxyPublicFunction } from '../_gateway'
 
 export default async function handler(req: any, res: any) {
-  return proxyPublicFunction(req, res, 'public-config', 4_000)
+  return proxyPublicFunction(req, res, 'public-config', 4_000, ['GET', 'POST'])
 }

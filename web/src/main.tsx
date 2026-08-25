0224ccb06ed7a0f422449f53b4d62a71c15d4377
thiagodownload <83071601+thiagodownload@users.noreply.github.com>
2026-08-25 00:25:26 -0300

    style: carregar estilos de MFA

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './auth.css'
+import './mfa.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
